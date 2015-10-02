8b09649560ec1a16d4d18fac8fc9176a0978be7a
Maximus <elendorial@gmail.com>
2015-10-03 01:30:56 +0300

    reindent

--- 1.1.29/BinarySearch.java
+++ 1.1.29/BinarySearch.java
@@ -6,73 +6,70 @@
 
     public class BinarySearch
     {
+        private BinarySearch() { }
 
-    private BinarySearch() { }
-
-    public static int indexOf(int key, int[] a, int lo, int hi)
-    {
-    	if(lo > hi)
-            return -1;
-        int mid = lo + (hi - lo) / 2;
-        if(key < a[mid])
-            return indexOf(key, a, lo, mid - 1);
-        else if(key > a[mid])
-            return indexOf(key, a, mid + 1, hi);
-        else
-            return mid;
-    }
-
-    public static int rank(int key, int[] a)
-    {
-
-        int val = indexOf(key, a, 0, a.length - 1);
-        int temp = -1;
-        while(val != -1)
+        public static int indexOf(int key, int[] a, int lo, int hi)
         {
-            temp = val;
-            val = indexOf(key, a, 0, val - 1);
+            if(lo > hi)
+                return -1;
+            int mid = lo + (hi - lo) / 2;
+            if(key < a[mid])
+                return indexOf(key, a, lo, mid - 1);
+            else if(key > a[mid])
+                return indexOf(key, a, mid + 1, hi);
+            else
+                return mid;
         }
 
-        return temp;
-
-    }
-
-    public static int count(int key, int[] a)
-    {
-        int val = indexOf(key, a, 0, a.length - 1);
-        int temp = val;
-        int count = 0;
-
-        while(val != -1)
+        public static int rank(int key, int[] a)
         {
-            count++;
-            val = indexOf(key, a, 0, val - 1);
+            int val = indexOf(key, a, 0, a.length - 1);
+            int temp = -1;
+            while(val != -1)
+            {
+                temp = val;
+                val = indexOf(key, a, 0, val - 1);
+            }
+            return temp;
+
         }
 
-        val = indexOf(key, a, temp + 1, a.length - 1);
-
-        while(val != -1)
+        public static int count(int key, int[] a)
         {
-            count++;
-            val = indexOf(key, a, val + 1, a.length - 1);
+            int val = indexOf(key, a, 0, a.length - 1);
+            int temp = val;
+            int count = 0;
+
+            while(val != -1)
+            {
+                count++;
+                val = indexOf(key, a, 0, val - 1);
+            }
+
+            val = indexOf(key, a, temp + 1, a.length - 1);
+
+            while(val != -1)
+            {
+                count++;
+                val = indexOf(key, a, val + 1, a.length - 1);
+            }
+
+            return count;
         }
 
-        return count;
-    }
+        public static void main(String[] args)
+        {
+            In in = new In(args[0]);
+            int[] whitelist = in.readAllInts();
 
-    public static void main(String[] args)
-    {
-      In in = new In(args[0]);
-      int[] whitelist = in.readAllInts();
+            Arrays.sort(whitelist);
 
-      Arrays.sort(whitelist);
-
-      while(!StdIn.isEmpty())
-      {
-         int key = StdIn.readInt();
-         System.out.println(count(key,whitelist));
-     }
-    }
+            while(!StdIn.isEmpty())
+            {
+                int key = StdIn.readInt();
+                System.out.println(count(key,whitelist));
+            }
+        }
     }
 
 
