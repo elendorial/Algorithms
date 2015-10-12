9f8089c6f0f40db1adae2ea724981f93c3092563
elendorial <elendorial@gmail.com>
2015-10-12 08:25:18 +0300

    added license

--- 1.1.35/A1_1_35.java
+++ 1.1.35/A1_1_35.java
@@ -1,3 +1,28 @@
+/*The MIT License (MIT)
+
+Copyright (c) 2015 Viktor Daropoulos
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+
 import edu.princeton.cs.algs4.StdRandom;
 
 
@@ -6,13 +31,13 @@ public class A1_1_35
 
     public static double[] dice()
     {
-        int SIDES = 6;
-        double[] dist = new double[2 * SIDES + 1];
-        for(int i = 1; i <= SIDES; i++)
-            for(int j = 1; j <= SIDES; j++)
+
+        double[] dist = new double[13];
+        for(int i = 1; i <= 6; i++)
+            for(int j = 1; j <= 6; j++)
                 dist[i + j] += 1.0;
 
-        for(int k = 2; k <= 2 * SIDES; k++)
+        for(int k = 2; k <= 12; k++)
             dist[k] /= 36.0;
 
         return dist;
@@ -20,8 +45,8 @@ public class A1_1_35
 
     public static double[] dice_throw(int N)
     {
-        int SIDES = 6;
-        double[] dist = new double[2 * SIDES + 1];
+
+        double[] dist = new double[13];
 
         for(int i = 0; i < N; i++)
         {
@@ -30,7 +55,7 @@ public class A1_1_35
             dist[dice1 + dice2] += 1.0;
         }
 
-        for(int k = 2; k <= 2 * SIDES; k++)
+        for(int k = 2; k <= 12; k++)
             dist[k] /= N;
 
         return dist;
